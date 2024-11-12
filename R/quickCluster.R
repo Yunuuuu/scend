828725670addba35ea9760ba00d26edf5bad3402
Yunuuuu <yunyunpp96@outlook.com>
2024-11-13 00:23:13 +0800

    gain n_hvgs and prop_hvgs arguments

--- R/quickCluster.R
+++ R/quickCluster.R
@@ -3,6 +3,9 @@
 #' @inheritDotParams clusterSNNGraph
 #' @inheritParams runPCA
 #' @inheritParams logNormCounts
+#' @param n_hvgs Integer specifying the number of top genes to retain.
+#' @param prop_hvgs Numeric scalar specifying the proportion of genes to report
+#' as HVGs.
 #' @param seed Integer scalar specifying the seed to use for the initial random
 #' vector in `IRLBA` and for `multi-level` or `Leiden` clustering.
 #' @seealso
@@ -15,7 +18,8 @@ quickCluster <- function(object, ...) UseMethod("quickCluster")
 .quickCluster <- function(
     object, size_factors = NULL,
     # `runPCA` arguments
-    d = 50L, scale = FALSE, subset_row = NULL,
+    d = 50L, scale = FALSE,
+    subset_row = NULL, n_hvgs = 500, prop_hvgs = 0.1,
     block_weight_policy = NULL,
     variable_block_weight = c(0, 1000),
     from_residuals = FALSE, extra_work = 7,
@@ -31,6 +35,13 @@ quickCluster <- function(object, ...) UseMethod("quickCluster")
 
     seed <- check_seed(seed)
     set_seed(seed)
+
+    if (is.null(subset_row)) {
+        fit <- modelGeneVar(object)
+        # At least 500 genes, or 10% of genes; whichever is larger.
+        subset_row <- getTopHVGs(fit, n = n_hvgs, prop = prop_hvgs)
+    }
+
     # dimensionality reduction
     object <- runPCA(
         object = object,
