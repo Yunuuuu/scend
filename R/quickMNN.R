6bd65dc1eb2c03e8fed444dfcd71f091c1c33a14
Yunuuuu <yunyunpp96@outlook.com>
2024-11-12 23:07:38 +0800

    rename the assay for batch normalized counts

--- R/quickMNN.R
+++ R/quickMNN.R
@@ -4,11 +4,15 @@
 #' @inheritParams runMNN
 #' @inheritParams logNormCounts
 #' @seealso
-#' - [runMNN]
+#' - [`logNormCounts`]
+#' - [`runPCA`]
+#' - [`runMNN`]
 #' @export
 quickMNN <- function(object, ...) UseMethod("quickMNN")
 
-.quickMNN <- function(
+#' @export
+#' @rdname quickMNN
+quickMNN.SingleCellExperiment <- function(
     object, block, size_factors = NULL,
     # `runPCA` arguments
     ...,
@@ -22,16 +26,18 @@ quickMNN <- function(object, ...) UseMethod("quickMNN")
     # nromalization, adjust for differences in sequencing depth
     object <- logNormCounts(
         object = object, block = block, threads = threads,
-        size_factors = size_factors
+        size_factors = size_factors,
+        name = "multiBatchNorm"
     )
 
     # dimensionality reduction
     object <- runPCA(
         object = object, block = block, ...,
+        assay = "multiBatchNorm",
         threads = threads, name = "PCA"
     )
 
-    # run MNN
+    # run MNN for batch correction
     runMNN(
         object = object, block = block, dimred = "PCA",
         k = k, n_mads = n_mads,
@@ -45,8 +51,39 @@ quickMNN <- function(object, ...) UseMethod("quickMNN")
 
 #' @export
 #' @rdname quickMNN
-quickMNN.SingleCellExperiment <- .quickMNN
+quickMNN.Seurat <- function(
+    object, block, size_factors = NULL,
+    # `runPCA` arguments
+    ...,
+    # `runMNN` arguments
+    k = 15L, n_mads = 3L,
+    robust_iterations = 2, robust_trim = 0.25,
+    mass_cap = NULL,
+    order = NULL, reference_policy = NULL,
+    BNPARAM = AnnoyParam(), threads = NULL,
+    name = "corrected") {
+    # nromalization, adjust for differences in sequencing depth
+    object <- logNormCounts(
+        object = object, block = block, threads = threads,
+        size_factors = size_factors,
+        name = "multiBatchNorm"
+    )
 
-#' @export
-#' @rdname quickMNN
-quickMNN.Seurat <- .quickMNN
+    # dimensionality reduction
+    object <- runPCA(
+        object = object, block = block, ...,
+        layer = "multiBatchNorm",
+        threads = threads, name = "PCA"
+    )
+
+    # run MNN for batch correction
+    runMNN(
+        object = object, block = block, dimred = "PCA",
+        k = k, n_mads = n_mads,
+        robust_iterations = robust_iterations,
+        robust_trim = robust_trim,
+        mass_cap = mass_cap,
+        order = order, reference_policy = reference_policy,
+        BNPARAM = BNPARAM, threads = threads, name = name
+    )
+}
