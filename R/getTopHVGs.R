8394adabdc0d20285bce3be1373fb85994bdf7ed
Yunuuuu <yunyunpp96@outlook.com>
2024-11-13 00:22:44 +0800

    gain prop argument

--- R/getTopHVGs.R
+++ R/getTopHVGs.R
@@ -1,6 +1,8 @@
 #' @inherit scrapper::chooseHighlyVariableGenes title description
 #' @inheritParams rlang::args_dots_used
 #' @param n Integer specifying the number of top genes to retain.
+#' @param prop Numeric scalar specifying the proportion of genes to report as
+#' HVGs.
 #' @param stats Numeric vector of variances (or a related statistic) across all
 #' genes. Typically the `residuals` from [`modelGeneVar`] are used here.
 #' @inheritParams scrapper::chooseHighlyVariableGenes
@@ -15,12 +17,21 @@ getTopHVGs <- function(stats, ...) {
 
 #' @export
 #' @rdname getTopHVGs
-getTopHVGs.scend_modelGeneVar <- function(stats, n = 2000,
-                                          larger = TRUE,
-                                          keep_ties = TRUE, ...) {
-    assert_number_whole(n)
+getTopHVGs.scend_modelGeneVar <- function(stats, n = NULL, prop = NULL,
+                                          larger = TRUE, keep_ties = TRUE,
+                                          ...) {
+    assert_number_whole(n, min = 0, allow_null = TRUE)
+    assert_number_decimal(prop, min = 0, max = 1, allow_null = TRUE)
     assert_bool(larger)
     assert_bool(keep_ties)
+    if (!is.null(n) && !is.null(prop)) {
+        n <- max(n, round(prop * nrow(stats)))
+    } else if (!is.null(n)) {
+    } else if (!is.null(prop)) {
+        n <- round(prop * nrow(stats))
+    } else {
+        n <- nrow(stats)
+    }
     scrapper::chooseHighlyVariableGenes(
         .subset2(stats, "residuals"),
         top = n, larger = larger, keep.ties = keep_ties
